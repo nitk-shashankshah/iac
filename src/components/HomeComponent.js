7217914f6debc5f093a3d96364672ca81303751d
Shashank Shah <nitk.shashankshah@gmail.com>
2020-01-24 19:29:00 +0530

    effects

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -15,8 +15,8 @@ class HomeComponent extends React.Component {
     super(props);
     this.state = { 
       images: [slider1, slider2, slider3, slider4, slider5,slider6, slider7],
-      widths: [150,150,150,250,150,150,150],
-      heights: [220,250,280,310,280,250,220],
+      widths: [150,150,150,278,150,150,150],
+      heights: [230,260,290,321,290,260,230],
       opacity: [0.5,0.5,0.5,1,0.5,0.5,0.5],
       tops: [-30,-20,-10,0,-10,-20,-30]
     };
@@ -24,27 +24,30 @@ class HomeComponent extends React.Component {
   }
 
   rollSlider(e) {
-    this.setState({widths: this.state.widths.map((val,ind) => (ind === parseInt(e.target.getAttribute('dataKey'))) ? 250 : 150)});
+    console.log(e.target.getAttribute('datakey'));
+    this.setState({widths: this.state.widths.map((val,ind) => (ind === parseInt(e.target.getAttribute('datakey'))) ? 278 : 150)});
     var hgts=this.state.heights;
+    console.log("Before:" + JSON.stringify(hgts) +" ||| " + JSON.stringify(this.state.widths));
     var tops=this.state.tops;
     var opacities=this.state.opacity;
     opacities = opacities.map(x=> {return 0.5});
-    opacities[parseInt(e.target.getAttribute('dataKey'))]=1;
+    opacities[parseInt(e.target.getAttribute('datakey'))]=1;
     var cnt=0;
-    for (var j=parseInt(e.target.getAttribute('dataKey')); j<this.state.images.length; j++) {
-      hgts[j] = 250-cnt*30;
+    for (var j=parseInt(e.target.getAttribute('datakey')); j<this.state.images.length; j++) {
+      hgts[j] = 321-cnt*30;
       tops[j] = -cnt*15;      
       cnt++;
     }
     cnt=0;
-    for (j=parseInt(e.target.getAttribute('dataKey')); j>=0; j--) {
-      hgts[j] = 250-cnt*30;      
+    for (j=parseInt(e.target.getAttribute('datakey')); j>=0; j--) {
+      hgts[j] = 321-cnt*30;      
       tops[j] = -cnt*15;
       cnt++;
     }
     this.setState({heights: hgts});    
     this.setState({tops: tops});
     this.setState({opacity: opacities});
+    console.log("After:" +JSON.stringify(hgts) + " ||| " + JSON.stringify(this.state.widths));
   }
 
   render() {
@@ -93,16 +96,16 @@ class HomeComponent extends React.Component {
           <ul><li>brertert</li><li>tsdfsdf</li><li>hjhkjhk</li></ul>
           </div>  
           </li>
-          <li><i class="fa fa-search" aria-hidden="true"></i></li>
-          <li><i class="fa fa-user" aria-hidden="true"></i></li>
-          <li><i class="fa fa-map-marker" aria-hidden="true"></i></li>
+          <li><i className="fa fa-search" aria-hidden="true"></i></li>
+          <li><i className="fa fa-user" aria-hidden="true"></i></li>
+          <li><i className="fa fa-map-marker" aria-hidden="true"></i></li>
         </ul>
         <img src={logo} className="App-logo" alt="logo" />    
       </header>
       <main>
         <div className="banner">
-          <div class="detailSection">
-            <h3 class="pageHeading">Leading Through Innovation</h3>
+          <div className="detailSection">
+            <h3 className="pageHeading">Leading Through Innovation</h3>
             <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione</p>
             <a href="more" class="btn primary">Know More</a>
             <a href="watchVideoBtn" class="btn activeBtn">Watch Video..</a>
@@ -114,13 +117,14 @@ class HomeComponent extends React.Component {
         </div>                
         <div className="slideshow-container">
           <h2>Our Capabilities</h2>
-          <a class="prev" onclick="plusSlides(-1, 0)">&#10094;</a>
+          <a class="prev">&#10094;</a>
           <div className="mySlides1">
             {this.state.images.map((item, index) => {
-              return <img src={item} key={index} dataKey={index} style={{ opacity: this.state.opacity[index], top: this.state.tops[index], width: this.state.widths[index], height: this.state.heights[index]}} onMouseOver={this.rollSlider} />
+              {{/*return <img src={item} key={index} dataKey={index} style={{ opacity: this.state.opacity[index], top: this.state.tops[index], width: this.state.widths[index], height: this.state.heights[index]}} onMouseOver={this.rollSlider} />*/}}              
+              {{return <div key={index} style={{ overflow:"hidden", opacity: this.state.opacity[index], background: "#fff", top: this.state.tops[index], width: this.state.widths[index], height: this.state.heights[index]}} ><img src={item} datakey={index} style={{opacity: this.state.opacity[index], height:"100%"}} onMouseOver={this.rollSlider}/></div>}}
             })}
           </div>
-          <a class="next" onclick="plusSlides(1, 0)">&#10095;</a>
+          <a class="next">&#10095;</a>
         </div>
       </main>
     </div>
