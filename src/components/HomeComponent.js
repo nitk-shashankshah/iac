ace2e9d11a590cea7251b2ba6b8892181ae01eac
Shashank Shah <nitk.shashankshah@gmail.com>
2020-02-03 19:50:06 +0530

    responsiveness

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -13,9 +13,13 @@ import search from '../assets/images/search.png';
 import chat from '../assets/images/chat.png';
 import locate from '../assets/images/location.png';
 import $ from 'jquery';
-import '../jquery.waterwheelCarousel.js'
+import '../jquery.waterwheelCarousel.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faKey } from '@fortawesome/free-solid-svg-icons';
 
-var Coverflow = require('react-coverflow');
+library.add(faBars, faKey);
+
 
 class HomeComponent extends React.Component {
   constructor(props) {
@@ -38,6 +42,7 @@ class HomeComponent extends React.Component {
   componentDidMount(){
     var carousel = $("#carousel").waterwheelCarousel({
       flankingItems: 3,
+      orientation: ((window.innerWidth > 1300) ? 'horizontal' : 'vertical'), 
       movingToCenter: function ($item) {
         $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
       },
@@ -103,35 +108,35 @@ class HomeComponent extends React.Component {
           </div>    
           </div>
           <div class="item"><a href="governance" onMouseOut={() => this.menuMouseOut()} onMouseOver={() => this.menuMouseOver(3)}>Governance</a>
-          <div className={this.state.openMenu === 3 ? "submenu governanceMenu" : "submenu"} onMouseOver={() => this.subMenuMouseOver(3)} onMouseOut={this.subMenuMouseOut}>
+          {/*<div className={this.state.openMenu === 3 ? "submenu governanceMenu" : "submenu"} onMouseOver={() => this.subMenuMouseOver(3)} onMouseOut={this.subMenuMouseOut}>
           <ul><li><b>IAC Overview</b></li><li><a href="/test">History</a></li><li><a href="/test">Global Capability Center</a></li><li><a href="/test">Leadership</a></li><li><a href="/test">Mission & Vision</a></li><li><a href="/test">Diversity & Inclusion</a></li><li><a href="/test">CSR</a></li><li><a href="/test">Innovation</a></li><li><a href="/test">Success Stories</a></li><li><a href="/test">FAQs</a></li></ul>
           <ul><li><b>News Articles</b></li><li><a href="/test">Top Stories</a></li><li><a href="/test">Newsletters</a></li><li><a href="/test">eMagaines</a></li></ul>
           <ul><li><b>Events</b></li><li><a href="/test">GDOP</a></li><li><a href="/test">Town Hall</a></li><li><a href="/test">Leadership visits</a></li></ul>
           <ul><li><b>Engagements</b></li><li><a href="/test">Employee Resource Groups</a></li><li><a href="/test">Testimonials</a></li><li><a href="/test">Business Integration</a></li></ul>
-          </div>    
+          </div>*/}   
           </div>
           <div class="item"><a href="products" onMouseOut={() => this.menuMouseOut()} onMouseOver={() => this.menuMouseOver(4)}>Engage Us</a>
-          <div className={this.state.openMenu === 4 ? "submenu engageMenu" : "submenu"} onMouseOver={() => this.subMenuMouseOver(4)} onMouseOut={this.subMenuMouseOut}>
+          {/*<div className={this.state.openMenu === 4 ? "submenu engageMenu" : "submenu"} onMouseOver={() => this.subMenuMouseOver(4)} onMouseOut={this.subMenuMouseOut}>
           <ul><li><b>IAC Overview</b></li><li><a href="/test">History</a></li><li><a href="/test">Global Capability Center</a></li><li><a href="/test">Leadership</a></li><li><a href="/test">Mission & Vision</a></li><li><a href="/test">Diversity & Inclusion</a></li><li><a href="/test">CSR</a></li><li><a href="/test">Innovation</a></li><li><a href="/test">Success Stories</a></li><li><a href="/test">FAQs</a></li></ul>
           <ul><li><b>News Articles</b></li><li><a href="/test">Top Stories</a></li><li><a href="/test">Newsletters</a></li><li><a href="/test">eMagaines</a></li></ul>
           <ul><li><b>Events</b></li><li><a href="/test">GDOP</a></li><li><a href="/test">Town Hall</a></li><li><a href="/test">Leadership visits</a></li></ul>
           <ul><li><b>Engagements</b></li><li><a href="/test">Employee Resource Groups</a></li><li><a href="/test">Testimonials</a></li><li><a href="/test">Business Integration</a></li></ul>
-          </div>   
+          </div>*/}   
           </div>
           <div class="item"><a href="products" onMouseOut={() => this.menuMouseOut()} onMouseOver={() => this.menuMouseOver(5)}>Quick Links</a>
-          <div className={this.state.openMenu === 5 ? "submenu quickLinksMenu" : "submenu"} onMouseOver={() => this.subMenuMouseOver(5)} onMouseOut={this.subMenuMouseOut}>
+          {/*<div className={this.state.openMenu === 5 ? "submenu quickLinksMenu" : "submenu"} onMouseOver={() => this.subMenuMouseOver(5)} onMouseOut={this.subMenuMouseOut}>
           <ul><li><b>IAC Overview</b></li><li><a href="/test">History</a></li><li><a href="/test">Global Capability Center</a></li><li><a href="/test">Leadership</a></li><li><a href="/test">Mission & Vision</a></li><li><a href="/test">Diversity & Inclusion</a></li><li><a href="/test">CSR</a></li><li><a href="/test">Innovation</a></li><li><a href="/test">Success Stories</a></li><li><a href="/test">FAQs</a></li></ul>
           <ul><li><b>News Articles</b></li><li><a href="/test">Top Stories</a></li><li><a href="/test">Newsletters</a></li><li><a href="/test">eMagaines</a></li></ul>
           <ul><li><b>Events</b></li><li><a href="/test">GDOP</a></li><li><a href="/test">Town Hall</a></li><li><a href="/test">Leadership visits</a></li></ul>
           <ul><li><b>Engagements</b></li><li><a href="/test">Employee Resource Groups</a></li><li><a href="/test">Testimonials</a></li><li><a href="/test">Business Integration</a></li></ul>
-          </div>    
+          </div>*/}    
           </div>       
           <div className="actionIcon"><a href="search"><img src={search} alt="logo" /></a></div>
           <div className="actionIcon"><a href="login"><img src={login} alt="logo" /></a></div>
           <div className="actionIcon locateus"><a href="locate"><img src={locate} alt="logo" /></a></div>
-          <img src={logo} className="extra-logo" alt="logo" />
+          <img src={logo} className="elanco-logo" alt="logo" />
           </div>
-          <div className="icon"><i className="fa fa-bars" onClick={this.rollMenu}></i></div>
+          <div className="icon"><FontAwesomeIcon icon="bars" onClick={this.rollMenu}/></div>
       </header>
       <main>
         <div className="banner">
@@ -144,24 +149,24 @@ class HomeComponent extends React.Component {
             </div>
           </div>
           <div class="chatBtn">          
-          <img src={chat} />
+          <img src={chat} alt="Chat with us"/>
             <p>Chat With Us</p>
           </div>
         </div>
         <div class="sliderSection">     
         <p className="capabilities">Our Capabilities</p>
         <div class="slider">
-        <a href="#" id="prev"></a>
+        <a href="previous" id="prev" alt="Previous"></a>
         <div id="carousel">
-          <a href="#"><div style={{"background":`url(${slider1})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-1"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
-          <a href="#"><div style={{"background":`url(${slider2})`,"background-position":"left bottom","width":"266px","height":"318px","background-size":"contain"}} id="item-2"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
-          <a href="#"><div style={{"background":`url(${slider3})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-3"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
-          <a href="#"><div style={{"background":`url(${slider4})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-4"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
-          <a href="#"><div style={{"background":`url(${slider5})`,"background-position":"left bottom","width":"268px","height":"320px","background-size":"contain"}} id="item-5"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
-          <a href="#"><div style={{"background":`url(${slider6})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-6"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
-          <a href="#"><div style={{"background":`url(${slider7})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-7"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>     
+          <a href="/eks" alt="Elanco Knowledge Solutions"><div style={{"background":`url(${slider1})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-1"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
+          <a href="/gfin" alt="Global Finance"><div style={{"background":`url(${slider2})`,"background-position":"left bottom","width":"266px","height":"318px","background-size":"contain"}} id="item-2"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
+          <a href="/operations" alt="Global Commercial Operations"><div style={{"background":`url(${slider3})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-3"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
+          <a href="/marketing" alt="Global Marketing"><div style={{"background":`url(${slider4})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-4"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
+          <a href="/it" alt="Information Technology"><div style={{"background":`url(${slider5})`,"background-position":"left bottom","width":"268px","height":"320px","background-size":"contain"}} id="item-5"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
+          <a href="/manufacturing" alt="Manufacturing"><div style={{"background":`url(${slider6})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-6"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>
+          <a href="/rnd" alt="Research and Development"><div style={{"background":`url(${slider7})`,"background-position":"left bottom","width":"273px","height":"318px","background-size":"contain"}} id="item-7"><p style={{"width": "100%","height": "100%","background":"rgb(255, 255, 255, 0.8)","padding":"0px","margin":"0px","display": "block","visibility": "visible","position": "absolute","z-index": "4","opacity": "0.8"}}></p></div></a>     
         </div>
-        <a href="#" id="next"></a>
+        <a href="next" id="next" alt="Next"></a>
         </div>
         </div> 
         </main>
